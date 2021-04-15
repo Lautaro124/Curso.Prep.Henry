ea4a4c87bcdca022b92ebc2c4ee1cf80ddf33b8f
Lautaro124 <59624470+Lautaro124@users.noreply.github.com>
2021-04-15 15:08:01 -0300

    Finalizacion no probada de el tercer punto

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -201,26 +201,41 @@ function esVerdadero(valor){
       return "Soy falso"
     }
   }
+  Coso()
 
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
-  
+  //Escribe tu código aquí  
+
+  for (var i = 1; i == 9; i++) {
+    return (i*6)
+  }
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if(numero>= 100 && numero < 1000){
+    return true
+  }else {
+    return false
+  }
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+
+  let contador= 1
+
+  while( contador != 8){
+    contador++
+    numero = numero + 5
+    }
 }
 
 
